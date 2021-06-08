f1ac26e061dd0b3d1532d907e260ec134493e90a
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2021-06-08 20:43:44 +0800

    feat: add spring data elasticsearch repository custom @Query demo

--- spring-data-es/src/main/java/com/github/learn/spring_data_es/infra/es/repo/declare/KnightIndexRepository.java
+++ spring-data-es/src/main/java/com/github/learn/spring_data_es/infra/es/repo/declare/KnightIndexRepository.java
@@ -3,6 +3,7 @@ package com.github.learn.spring_data_es.infra.es.repo.declare;
 import com.github.learn.spring_data_es.infra.es.entity.Knight;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 /**
@@ -19,5 +20,45 @@ public interface KnightIndexRepository extends ElasticsearchRepository<Knight, L
      */
     Optional<Knight> findByAccount_KnightId(Long knightId);
 
+    /**
+     * 按 knightId subId 搜索
+     *
+     * @param knightId knightId
+     * @param subId subId
+     * @return Knight
+     */
+    @Query("{\n"
+        + "  \"bool\": {\n"
+        + "    \"filter\": [\n"
+        + "      {\n"
+        + "        \"nested\": {\n"
+        + "          \"path\": \"account\",\n"
+        + "          \"query\": {\n"
+        + "            \"bool\": {\n"
+        + "              \"filter\": [\n"
+        + "                {\n"
+        + "                  \"terms\": {\n"
+        + "                    \"account.knightId\": [\n"
+        + "                      \"?0\"\n"
+        + "                    ]\n"
+        + "                  }\n"
+        + "                },\n"
+        + "                {\n"
+        + "                  \"terms\": {\n"
+        + "                    \"account.subId\": [\n"
+        + "                      \"?1\"\n"
+        +
+        "                    ]\n"
+        + "                  }\n"
+        + "                }\n"
+        + "              ]\n"
+        + "            }\n"
+        + "          }\n"
+        + "        }\n"
+        + "      }\n"
+        + "    ]\n"
+        + "  "
+        + "}\n"
+        + "}")
     List<Knight> findByAccount_KnightIdAndAccount_SubId(long knightId, long subId);
 }

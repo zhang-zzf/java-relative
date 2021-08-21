16b38217883365772c09b15627825acb2004e05f
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2021-08-21 17:25:51 +0800

    feat: add StringReverse
    https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2y0c2/

---
+++ leetcode/src/test/java/com/github/learn/leetcode/domain/service/common/StringReverseTest.java
@@ -0,0 +1,26 @@
+package com.github.learn.leetcode.domain.service.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+/**
+ * @author zhanfeng.zhang
+ * @date 2021/08/21
+ */
+@SpringBootTest
+class StringReverseTest {
+
+    @Autowired
+    StringReverse stringReverse;
+
+    @Test
+    void given_when_then() {
+        char[] chars = "hello".toCharArray();
+        stringReverse.reverse(chars);
+        then(chars).containsExactly('o', 'l', 'l', 'e', 'h');
+    }
+
+}

0a3fb02d932a85346458f4166948e3966f14b257
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-02-18 15:16:43 +0800

    feat: add leetcode algorithm
    - 1.两数之和

---
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/TwoSumTest.java
@@ -0,0 +1,86 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 只会存在一个有效答案 
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+// Related Topics 数组 哈希表 👍 13450 👎 0
+
+
+package com.shuzijun.leetcode.editor.cn;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+
+public class TwoSumTest {
+
+    final Solution solution = new Solution();
+
+    @Test
+    void givenNormal_when_thenSuccess() {
+        then(solution.twoSum(new int[]{3, 2, 4}, 6)).contains(1, 2);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> numToIdx = new HashMap<>(nums.length);
+            for (int i = 0; i < nums.length; i++) {
+                final Integer existSub = numToIdx.getOrDefault(target - nums[i], null);
+                if (existSub != null) {
+                    return new int[]{existSub.intValue(), i};
+                } else {
+                    numToIdx.put(nums[i], i);
+                }
+            }
+            // never go here
+            return new int[0];
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}

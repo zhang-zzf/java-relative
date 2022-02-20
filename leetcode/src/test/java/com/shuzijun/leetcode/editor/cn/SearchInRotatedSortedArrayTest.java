56b187f5f3ac12c9289dbbe3edf0a15a2dac43bd
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-02-20 12:18:30 +0800

    feat: add leetcode algorithm 二分查找
    - 69. x 的平方根
    - 374. 猜数字大小
    - 33. 搜索旋转排序数组

---
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/SearchInRotatedSortedArrayTest.java
@@ -0,0 +1,60 @@
+
+package com.shuzijun.leetcode.editor.cn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+
+public class SearchInRotatedSortedArrayTest {
+
+    final Solution solution = new Solution();
+
+    @Test
+    void givenNormal_when_thenSuccess() {
+        final int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        then(solution.search(nums, 4)).isEqualTo(0);
+        then(solution.search(nums, 5)).isEqualTo(1);
+        then(solution.search(nums, 7)).isEqualTo(3);
+        then(solution.search(nums, 0)).isEqualTo(4);
+        then(solution.search(nums, 1)).isEqualTo(5);
+        then(solution.search(nums, 2)).isEqualTo(6);
+        then(solution.search(nums, 3)).isEqualTo(-1);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int search(int[] nums, int target) {
+            int ans = -1;
+            int left = 0, right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if (nums[mid] == target) {
+                    ans = mid;
+                    break;
+                } else {
+                    if (nums[0] <= nums[mid]) {
+                        // 左侧有序
+                        if (target < nums[mid] && target >= nums[0]) {
+                            right = mid - 1;
+                        } else {
+                            left = mid + 1;
+                        }
+                    } else {
+                        if (target > nums[mid] && target <= nums[nums.length - 1]) {
+                            left = mid + 1;
+                        } else {
+                            right = mid - 1;
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}

fa17122a05f9df3382c6014e14b866e9565c7103
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-02-23 17:16:17 +0800

    feat: add leetcode algorithm 树专题
    - 113. 路径总和 II

---
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,27 @@
+package com.shuzijun.leetcode.editor.cn;
+
+/**
+ * @author 张占峰 (Email: zhang.zzf@alibaba-inc.com / ID: 235668)
+ * @date 2022/2/23
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+
+}

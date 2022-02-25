e43eb2a946a9b2c05451ade588401cc2d5e0ede0
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-02-25 14:41:30 +0800

    feat: add leetcode algorithm 树专题
    - 116. 填充每个节点的下一个右侧节点指针

---
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/PopulatingNextRightPointersInEachNodeTest.java
@@ -0,0 +1,168 @@
+//给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下： 
+//
+// 
+//struct Node {
+//  int val;
+//  Node *left;
+//  Node *right;
+//  Node *next;
+//} 
+//
+// 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。 
+//
+// 初始状态下，所有 next 指针都被设置为 NULL。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [1,2,3,4,5,6,7]
+//输出：[1,#,2,3,#,4,5,6,7,#]
+//解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化的输出按层序遍历排列，同一层节点由 
+//next 指针连接，'#' 标志着每一层的结束。
+// 
+//
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 2¹² - 1] 范围内 
+// -1000 <= node.val <= 1000 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你只能使用常量级额外空间。 
+// 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 链表 二叉树 👍 684 👎 0
+
+
+package com.shuzijun.leetcode.editor.cn;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+
+public class PopulatingNextRightPointersInEachNodeTest {
+
+    final Solution solution = new Solution();
+
+    @Test
+    void givenNormal_when_thenSuccess() {
+        final Node root = new Node(1,
+                new Node(2,
+                        new Node(4),
+                        new Node(5),
+                        null),
+                new Node(3,
+                        new Node(6),
+                        new Node(7),
+                        null),
+                null
+        );
+        final Node node = solution.connect(root);
+        then(node.next).isNull();
+    }
+
+    /**
+     * [0]
+     */
+    @Test
+    void giveFailedCaseOne_when_thenSuccess() {
+        final Node root = new Node(0, null, null, null);
+        final Node node = solution.connect(root);
+        then(node.next).isNull();
+    }
+
+    class Node {
+
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+
+    }
+
+    ;
+    //leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+    class Solution {
+
+        public Node connect(Node root) {
+            if (root == null) {
+                return null;
+            }
+            if (root.left == null || root.right == null) {// 完美2叉树的规则
+                return root;
+            }
+            root.left.next = root.right;
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
+            connect(root.left);
+            connect(root.right);
+            return root;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}

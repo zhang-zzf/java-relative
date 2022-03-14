ffc20f575e77917b6145969d23377d3e93e5bc78
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-03-14 18:50:35 +0800

    feat: add leetcode algorithm 70. 爬楼梯

---
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/ClimbingStairsTest.java
@@ -0,0 +1,43 @@
+
+package com.shuzijun.leetcode.editor.cn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+
+class ClimbingStairsTest {
+
+    final Solution solution = new Solution();
+
+    @Test
+    void givenNormal_when_thenSuccess() {
+        then(solution.climbStairs(2)).isEqualTo(2);
+        then(solution.climbStairs(3)).isEqualTo(3);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            if (n == 2) {
+                return 2;
+            }
+            int n2 = 1, n1 = 2;
+            int ans = 0;
+            for (int i = 3; i <= n; i++) {
+                ans = n2 + n1;
+                n2 = n1;
+                n1 = ans;
+            }
+            return ans;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}

cba5e4001d9c3fb5998350528f176bd66a6a58fc
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-03-21 19:49:58 +0800

    feat: optimize leetcode algorithm DP
    - 121. 买卖股票的最佳时机

--- leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/BestTimeToBuyAndSellStockTest.java
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/BestTimeToBuyAndSellStockTest.java
@@ -14,20 +14,20 @@ class BestTimeToBuyAndSellStockTest {
     void givenNormal_when_thenSuccess() {
         then(solution.maxProfit(new int[]{7, 1, 5, 3, 6, 4})).isEqualTo(5);
         then(solution.maxProfit(new int[]{7, 6, 4, 3, 1})).isEqualTo(0);
+        // fail case 1
+        then(solution.maxProfit(new int[]{2, 4, 1})).isEqualTo(2);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
         public int maxProfit(int[] prices) {
-            int min = prices[0], max = min, ans = 0;
+            int minPrice = prices[0], ans = 0;
             for (int i = 0; i < prices.length; i++) {
-                if (prices[i] < min) {
-                    max = min = prices[i];
-                } else if (prices[i] > max) {
-                    max = prices[i];
-                    ans = Math.max(ans, max - min);
+                if (prices[i] < minPrice) {
+                    minPrice = prices[i];
                 }
+                ans = Math.max(ans, prices[i] - minPrice);
             }
             return ans;
         }

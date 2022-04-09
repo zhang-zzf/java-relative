0c5a2adb964e4453c9167398e9b734efe678b0bb
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-04-09 19:05:19 +0800

    feat: add docker java hello world

---
+++ hello-world/src/test/java/com/github/learn/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.github.learn;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@Slf4j
+public class ApplicationTest {
+
+    @Test
+    public void givenOne_thenEqualsToOne_thenTrue() {
+        then(1).isEqualTo(1);
+    }
+
+}

58f3c017dcf4736bec17500558541e1ee6f31335
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-04-30 10:28:00 +0800

    feat: add dp algorithm
    - 238. 除自身以外数组的乘积

--- leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/ProductOfArrayExceptSelfTest.java
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/ProductOfArrayExceptSelfTest.java
@@ -12,8 +12,8 @@ class ProductOfArrayExceptSelfTest {
 
     @Test
     void givenNormal_when_thenSuccess() {
-        final int[] ints = solution.productExceptSelf(new int[]{-1, 1, 0, -3, 3});
-        then(ints).containsExactly(0, 0, 9, 0, 0);
+        then(solution.productExceptSelf(new int[]{-1, 1, 0, -3, 3})).containsExactly(0, 0, 9, 0, 0);
+        then(solution.productExceptSelf(new int[]{1, 2, 3, 4})).containsExactly(24, 12, 8, 6);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -21,17 +21,14 @@ class ProductOfArrayExceptSelfTest {
 
         public int[] productExceptSelf(int[] nums) {
             final int lng = nums.length;
-            int[] left = new int[lng];
-            int[] right = new int[lng];
-            for (int i = 0; i < lng; i++) {
-                left[i] = i == 0 ? 1 : left[i - 1] * nums[i - 1];
-            }
-            for (int i = lng - 1; i >= 0; i--) {
-                right[i] = (i + 1) == lng ? 1 : right[i + 1] * nums[i + 1];
-            }
             int[] ans = new int[lng];
             for (int i = 0; i < lng; i++) {
-                ans[i] = left[i] * right[i];
+                ans[i] = i == 0 ? 1 : ans[i - 1] * nums[i - 1];
+            }
+            int right = 1;
+            for (int i = lng - 1; i >= 0; i--) {
+                right *= (i + 1) == lng ? 1 : nums[i + 1];
+                ans[i] *= right;
             }
             return ans;
         }

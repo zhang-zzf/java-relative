a2c9d73c6bc6c2bc3923f85d34092ce3f3b46fcc
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-05-28 11:43:14 +0800

    feat: optimize leetcode 712

--- leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/MinimumAsciiDeleteSumForTwoStringsTest.java
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/MinimumAsciiDeleteSumForTwoStringsTest.java
@@ -27,27 +27,27 @@ class MinimumAsciiDeleteSumForTwoStringsTest {
     class Solution {
 
         public int minimumDeleteSum(String s1, String s2) {
-            final int rLng = s1.length();
-            final int cLng = s2.length();
-            int[][] dp = new int[rLng + 1][cLng + 1];
-            for (int i = cLng - 1; i >= 0; i--) {
-                dp[rLng][i] = dp[rLng][i + 1] + s2.charAt(i);
-            }
-            for (int i = rLng - 1; i >= 0; i--) {
-                dp[i][cLng] = dp[i + 1][cLng] + s1.charAt(i);
-            }
-            for (int i = rLng - 1; i >= 0; i--) {
+            final int s1Lng = s1.length();
+            final int s2Lng = s2.length();
+            int[][] dp = new int[s1Lng + 1][s2Lng + 1];
+            for (int i = 0; i < s1Lng; i++) {
                 final char c1 = s1.charAt(i);
-                for (int j = cLng - 1; j >= 0; j--) {
+                for (int j = 0; j < s2Lng; j++) {
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
                     final char c2 = s2.charAt(j);
                     if (c1 == c2) {
-                        dp[i][j] = dp[i + 1][j + 1];
-                    } else {
-                        dp[i][j] = Math.min(dp[i][j + 1] + c2, dp[i + 1][j] + c1);
+                        dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + c2);
                     }
                 }
             }
-            return dp[0][0];
+            int sum1 = 0, sum2 = 0;
+            for (int i = 0; i < s1Lng; i++) {
+                sum1 += s1.charAt(i);
+            }
+            for (int i = 0; i < s2Lng; i++) {
+                sum2 += s2.charAt(i);
+            }
+            return (sum1 - dp[s1Lng][s2Lng]) + (sum2 - dp[s1Lng][s2Lng]);
         }
 
     }

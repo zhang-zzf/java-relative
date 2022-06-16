f90cb66d91ce59297f1df1e33f172e64a664124b
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-06-16 19:14:27 +0800

    feat: add spring-web

---
+++ spring-web/src/main/java/com/github/learn/springweb/controller/StringBodyPostController.java
@@ -0,0 +1,25 @@
+package com.github.learn.springweb.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhanfeng.zhang
+ * @date 2022/06/15
+ */
+@Controller
+@RestController
+@Slf4j
+public class StringBodyPostController {
+
+    @PostMapping("/api_1")
+    public Object api_1(@RequestHeader("Access-Token") String token, @RequestBody String body) {
+        log.info("post request:{}, {}", token, body);
+        return body;
+    }
+
+}
---
+++ spring-web/src/test/java/com/github/learn/springweb/controller/StringBodyPostControllerTest.java
@@ -0,0 +1,9 @@
+package com.github.learn.springweb.controller;
+
+/**
+ * @author zhanfeng.zhang
+ * @date 2022/06/15
+ */
+public class StringBodyPostControllerTest {
+
+}

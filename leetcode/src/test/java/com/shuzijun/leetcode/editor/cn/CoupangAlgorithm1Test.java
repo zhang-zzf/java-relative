08f1e8b478e435ee4dbf2a7427470db0a130fba4
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-12-14 21:36:49 +0800

    feat: review +-*/ algorithm

--- leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/CoupangAlgorithm1Test.java
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/CoupangAlgorithm1Test.java
@@ -2,7 +2,8 @@ package com.shuzijun.leetcode.editor.cn;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
@@ -12,129 +13,63 @@ public class CoupangAlgorithm1Test {
     @Test
     void given_when_then() {
         Solution solution = new Solution();
+        then(solution.calc("2")).isEqualTo(2);
         then(solution.calc("2+3*5-4/2")).isEqualTo(15);
         then(solution.calc("2+3*5*1-4/2")).isEqualTo(15);
         then(solution.calc("2+13*5-4/2")).isEqualTo(65);
         then(solution.calc("2+13*5*2-4/2")).isEqualTo(130);
         then(solution.calc("2+13*5*2-4/2/2")).isEqualTo(131);
-        then(solution.calc("(2+3)*5-4/2")).isEqualTo(23);
-        then(solution.calc("(2+(3+5)*2)*5-4/2")).isEqualTo(88);
+        // then(solution.calc("(2+3)*5-4/2")).isEqualTo(23);
+        // then(solution.calc("(2+(3+5)*2)*5-4/2")).isEqualTo(88);
     }
 
     class Solution {
 
+        /**
+         * 仅计算 + - * /
+         */
         public int calc(String str) {
-            char[] chars = str.replaceAll(" ", "").toCharArray();
-            Map<Integer, Integer> opToPriority = new HashMap<Integer, Integer>() {{
-                put((int) '(', 0);
-                put((int) ')', 0);
-                put((int) '*', 1);
-                put((int) '/', 1);
-                put((int) '+', 2);
-                put((int) '-', 2);
-            }};
-            Queue<Integer> middleExpr = parseMiddleExpr(chars, opToPriority.keySet());
-            // 后缀表达式
-            Queue<Integer> suffix = convertMiddleExprToSuffixExpr(middleExpr, opToPriority);
-            Stack<Integer> stack = new Stack<>();
-            int x = 0, y = 0;
-            while (!suffix.isEmpty()) {
-                Integer poll = suffix.poll();
-                switch (poll) {
-                    case (int) '*':
-                        stack.push(stack.pop() * stack.pop());
-                        break;
-                    case (int) '/':
-                        y = stack.pop();
-                        x = stack.pop();
-                        stack.push(x / y);
-                        break;
-                    case (int) '+':
-                        stack.push(stack.pop() + stack.pop());
-                        break;
-                    case (int) '-':
-                        y = stack.pop();
-                        x = stack.pop();
-                        stack.push(x - y);
-                        break;
-                    default:
-                        stack.push(poll);
-                }
-            }
-            return stack.peek();
-        }
-
-        private Queue<Integer> convertMiddleExprToSuffixExpr(Queue<Integer> middleExpr,
-                                                             Map<Integer, Integer> opToPriority) {
-            Set<Integer> opSet = opToPriority.keySet();
-            Queue<Integer> ans = new LinkedList<>();
-            Stack<Integer> opStack = new Stack<>();
-            while (!middleExpr.isEmpty()) {
-                Integer poll = middleExpr.poll();
-                if (!opSet.contains(poll)) {
-                    ans.offer(poll);
+            Deque<Integer> deque = parseMiddleExpr(str);
+            Deque<Integer> stack = new LinkedList<>();
+            while (!deque.isEmpty()) {
+                Integer i = deque.poll();
+                if (i == '*') {
+                    stack.offer(stack.pollLast() * deque.poll());
+                } else if (i == '/') {
+                    stack.offer(stack.pollLast() / deque.poll());
                 } else {
-                    if ((int) ')' == poll) {
-                        // ')'
-                        Integer op;
-                        while ((op = opStack.pop()) != (int) '(') {
-                            ans.offer(op);
-                        }
-                    } else if ((int) '(' == poll) {
-                        // '(' 直接压栈
-                        opStack.push(poll);
-                    } else {
-                        while (true) {
-                            Integer topOp = (opStack.empty() ? null : opStack.peek());
-                            if (comparePriority(poll, topOp, opToPriority) > 0) {
-                                // 优先级更高
-                                opStack.push(poll);
-                                break;
-                            } else {
-                                // 小于等于栈顶的优先级
-                                ans.offer(opStack.pop());
-                            }
-                        }
-                    }
+                    stack.offer(i);
                 }
             }
-            // 处理结尾数据
-            while (!opStack.empty()) {
-                ans.offer(opStack.pop());
-            }
-            return ans;
-        }
-
-        private int comparePriority(Integer poll, Integer topOp, Map<Integer, Integer> opToPriority) {
-            if (topOp == null) {
-                return 1;
-            }
-            if (topOp == (int) '(') {
-                return 1;
-            }
-            if (opToPriority.get(poll) < opToPriority.get(topOp)) {
-                return 1;
-            }
-            return -1;
-        }
-
-        private Queue<Integer> parseMiddleExpr(char[] chars, Set<Integer> opSet) {
-            Queue<Integer> ans = new LinkedList<>();
-            int num = 0;
-            for (int aChar : chars) {
-                if (opSet.contains(aChar)) {
-                    if (num != 0) {
-                        ans.offer(num);
-                        num = 0;
-                    }
-                    ans.offer(aChar);
+            while (!stack.isEmpty()) {
+                Integer i = stack.poll();
+                if (i == '+') {
+                    deque.offer(deque.pollLast() + stack.poll());
+                } else if (i == '-') {
+                    deque.offer(deque.pollLast() - stack.poll());
                 } else {
-                    num = num * 10 + (aChar - '0');
+                    deque.offer(i);
                 }
             }
-            // the last num
-            if (num != 0) {
-                ans.offer(num);
+            return deque.peek();
+        }
+
+        private Deque<Integer> parseMiddleExpr(String str) {
+            Deque<Integer> ans = new LinkedList<>();
+            int left = 0, right = 0;
+            while (right < str.length()) {
+                char c = str.charAt(right);
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
+                    if (left < right) {
+                        ans.offer(Integer.valueOf(str.substring(left, right)));
+                    }
+                    ans.offer((int) c);
+                    left = right + 1;
+                }
+                right += 1;
+            }
+            if (left < right) {
+                ans.offer(Integer.valueOf(str.substring(left, right)));
             }
             return ans;
         }

e1465736085aae692757b36b1c01522326f9a42f
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2022-12-16 23:20:31 +0800

    feat: review leetcode algorithm 704. 二分查找

--- leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/BinarySearchTest.java
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/BinarySearchTest.java
@@ -3,6 +3,8 @@ package com.shuzijun.leetcode.editor.cn;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.BDDAssertions.then;
+
 
 public class BinarySearchTest {
 
@@ -10,33 +12,39 @@ public class BinarySearchTest {
 
     @Test
     void givenNormal_when_thenSuccess() {
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        then(solution.search(nums, 9)).isEqualTo(4);
+    }
 
+    @Test
+    void givenNotExists_when_thenReturnNegativeOne() {
+        int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
+        then(solution.search(nums, 2)).isEqualTo(-1);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
 
         public int search(int[] nums, int target) {
-            // 定义值域 [-1, nums.length-1]
-            int ret = -1;
+            int ans = -1;
             int left = 0, right = nums.length - 1;
             while (left <= right) {
                 int mid = left + ((right - left) >> 1);
-                int value = getV(nums, mid);
-                if (value == target) {
-                    ret = mid;
+                int val = getV(nums, mid);
+                if (val == target) {
+                    ans = mid;
                     break;
-                } else if (value < target) {
-                    left = mid + 1;
-                } else {
+                } else if (target < val) {
                     right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
-            return ret;
+            return ans;
         }
 
-        private int getV(int[] nums, int idx) {
-            return nums[idx];
+        private int getV(int[] nums, int mid) {
+            return nums[mid];
         }
 
     }

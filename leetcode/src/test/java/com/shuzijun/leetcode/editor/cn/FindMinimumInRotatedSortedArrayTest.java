9069baaf9764cc36c34879a581c937fbfe7497b2
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2023-02-28 22:12:45 +0800

    feat: add leetcode algorithm 153

--- leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/FindMinimumInRotatedSortedArrayTest.java
+++ leetcode/src/test/java/com/shuzijun/leetcode/editor/cn/FindMinimumInRotatedSortedArrayTest.java
@@ -71,15 +71,17 @@ public class FindMinimumInRotatedSortedArrayTest {
 
         public int findMin(int[] nums) {
             int left = 0, right = nums.length - 1;
-            while (left < right) {
-                int mid = left + ((right - left) / 2);
-                if (nums[mid] > nums[right]) {
-                    left = mid + 1;
+            int ans = nums[0];
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                ans = Math.min(ans, nums[mid]);
+                if (nums[mid] < nums[right]) {
+                    right = mid - 1;
                 } else {
-                    right = mid;
+                    left = mid + 1;
                 }
             }
-            return nums[left];
+            return ans;
         }
 
     }

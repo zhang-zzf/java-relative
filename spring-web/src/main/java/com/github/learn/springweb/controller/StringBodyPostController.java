2be9a987c4e451c545e919894adf7339060be0cc
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2023-10-13 11:15:57 +0800

    feat: add spring-web

--- spring-web/src/main/java/com/github/learn/springweb/controller/StringBodyPostController.java
+++ spring-web/src/main/java/com/github/learn/springweb/controller/StringBodyPostController.java
@@ -2,24 +2,24 @@ package com.github.learn.springweb.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * @author zhanfeng.zhang
- * @date 2022/06/15
+ * 以 String 接收 POST 的 Body
  */
 @Controller
 @RestController
 @Slf4j
+@RequestMapping("/")
 public class StringBodyPostController {
 
-  @PostMapping("/api_1")
-  public Object api_1(@RequestHeader("Access-Token") String token, @RequestBody String body) {
-    log.info("post request:{}, {}", token, body);
-    return body;
-  }
+
+    @PostMapping("/api_1")
+    public Object api_1(
+            @RequestHeader(name = "Access-Token", required = false) String token,
+            @RequestBody(required = false) String body) {
+        log.info("post request:{}, {}", token, body);
+        return body;
+    }
 
 }

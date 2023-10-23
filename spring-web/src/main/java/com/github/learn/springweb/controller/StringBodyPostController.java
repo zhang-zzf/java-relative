493ef43b63024476b83d440d0df2e6a8f141af5e
zzf.zhang <zhangzhanfeng@icloud.com>
2023-10-23 19:27:42 +0800

    feat: add ControllerCases

--- spring-web/src/main/java/com/github/learn/springweb/controller/StringBodyPostController.java
+++ spring-web/src/main/java/com/github/learn/springweb/controller/StringBodyPostController.java
@@ -2,7 +2,11 @@ package com.github.learn.springweb.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 以 String 接收 POST 的 Body
@@ -14,12 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class StringBodyPostController {
 
 
-    @PostMapping("/api_1")
-    public Object api_1(
-            @RequestHeader(name = "Access-Token", required = false) String token,
-            @RequestBody(required = false) String body) {
-        log.info("post request:{}, {}", token, body);
-        return body;
-    }
+  @PostMapping("/api_1")
+  public Object api_1(
+      @RequestHeader(name = "Access-Token", required = false) String token,
+      @RequestBody(required = false) String body) {
+    log.info("post request:{}, {}", token, body);
+    return body;
+  }
+
+  @PostMapping("/api_2")
+  public Object api_2(
+      @RequestHeader(name = "Access-Token", required = false) String token,
+      @RequestBody(required = false) byte[] body) {
+    log.info("post request:{}, {}", token, body);
+    return body;
+  }
+
 
 }

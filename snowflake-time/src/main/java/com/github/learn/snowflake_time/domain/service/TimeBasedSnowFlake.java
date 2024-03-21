491398e310afa91607abed874ae48e5891a66686
zzf.zhang <zhangzhanfeng@icloud.com>
2024-03-21 15:31:56 +0800

    feat: add spring-framework demo

--- snowflake-time/src/main/java/com/github/learn/snowflake_time/domain/service/TimeBasedSnowFlake.java
+++ snowflake-time/src/main/java/com/github/learn/snowflake_time/domain/service/TimeBasedSnowFlake.java
@@ -1,10 +1,8 @@
 package com.github.learn.snowflake_time.domain.service;
 
-import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -16,20 +14,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class TimeBasedSnowFlake {
 
+  public static final DateTimeFormatter YYYY_MM_DD_HH_MM_SS = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
   /**
    * generate id use time
    * <p>不保证 id 不重复</p>
    * <p>${yyyyMMddHHmmss} * 100000 + ${ms} + new Random().nextInt(99000)</p>
-   * <p>sample: 202108182202566288</p>
+   * <p>sample: 20210818 220256 6288</p>
    *
    * @return id
    */
   public long generate() {
-    final LocalDateTime now = LocalDateTime.now();
-    final String time = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").format(now);
-    final long id =
-        Long.parseLong(time) * 100000 + now.get(MILLI_OF_SECOND) + new Random().nextInt(99000);
-    return id;
+    final String time = YYYY_MM_DD_HH_MM_SS.format(LocalDateTime.now());
+    return Long.parseLong(time) * 100000 + ThreadLocalRandom.current().nextInt(99999);
   }
 
 }

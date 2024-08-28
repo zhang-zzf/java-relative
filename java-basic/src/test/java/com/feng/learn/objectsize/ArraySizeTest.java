89148d09c056075654f383e557a1a03345420879
zzf.zhang <zhangzhanfeng@icloud.com>
2024-08-28 15:01:35 +0800

    feat: add object memory layout

--- java-basic/src/test/java/com/feng/learn/objectsize/ArraySizeTest.java
+++ java-basic/src/test/java/com/feng/learn/objectsize/ArraySizeTest.java
@@ -1,50 +1,95 @@
 package com.feng.learn.objectsize;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.Test;
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+import static java.lang.System.out;
 
 /**
  * 测量数组对象头的大小
- * <p>测试环境jdk1.8</p>
- * <p>测试1: new 100W个 只有一个8个长度的int[]，然后观察heap</p>
- * <p>测试2：new 100W个 只有一个8个长度的long[]，然后观察heap</p>
- * <p>得出结论: 每个数组对象头是20byte + 4byte内存填充 + 实际占用内存</p>
+ * <p>测试环境jdk17，默认 jdk 启动参数 -XX:+UseCompressedOops -XX:FieldsAllocationStyle -XX +CompactFields</p>
+ * <pre>
+ * # VM mode: 64 bits
+ * # Compressed references (oops): 3-bit shift
+ * # Compressed class pointers: 0-bit shift and 0x800000000 base
+ * # Object alignment: 8 bytes
+ * #                       ref, bool, byte, char, shrt,  int,  flt,  lng,  dbl
+ * # Field sizes:            4,    1,    1,    2,    2,    4,    4,    8,    8
+ * # Array element sizes:    4,    1,    1,    2,    2,    4,    4,    8,    8
+ * # Array base offsets:    16,   16,   16,   16,   16,   16,   16,   16,   16
+ * </pre>
+ *
+ * <pre>
+ * # VM mode: 64 bits
+ * # Compressed references (oops): disabled
+ * # Compressed class pointers: 0-bit shift and 0x800000000 base
+ * # Object alignment: 8 bytes
+ * #                       ref, bool, byte, char, shrt,  int,  flt,  lng,  dbl
+ * # Field sizes:            8,    1,    1,    2,    2,    4,    4,    8,    8
+ * # Array element sizes:    8,    1,    1,    2,    2,    4,    4,    8,    8
+ * # Array base offsets:    16,   16,   16,   16,   16,   16,   16,   16,   16
+ * </pre>
+ *
+ * <pre>
+ *     注意点：关闭对象指针后，对象头中的类指针占用 4 byte 而不是 8 byte
+ * </pre>
  */
 public class ArraySizeTest {
 
     /**
-     * 分析heap100W = 56000000byte。
-     * <p>一个数组对象占用56byte
-     * <p>对象头和对齐填充56-32=24byte</p>
-     * <p>对象头 20bytes = 8(类引用) + 8(头) + 4(数组大小)</p>
-     * <p>对象8字节对齐填充 = 4byte</p>
+     * <p>对象头 16 byte = 8(类引用) + 4(头) + 4(数组大小)</p>
+     * <p>对象实例 =  4*n byte</p>
+     * <p>对象 padding = 0 byte or 4 byte </p>
      */
     @Test
-    public void testIntArraySize() throws InterruptedException {
-        int size = 1000000;
-        List<int[]> tmp = new ArrayList<>(size);
-        for (int i = 0; i < size; i++) {
-            tmp.add(new int[8]); //拥有8个int的数组，每个int占用4个byte字节，共32byte
+    public void testIntArraySize() {
+        out.println(VM.current().details());
+        for (int i = 0; i <= 8; i++) {
+            out.println(ClassLayout.parseInstance(new int[i]).toPrintable());
         }
-        Thread.currentThread().join();
     }
 
     /**
-     * 分析heap100W = 88000000byte。
-     * <p>一个数组对象占用88byte
-     * <p>对象头和对齐填充88-64=24byte</p>
-     * <p>对象头 20bytes = 8(类引用) + 8(头) + 4(数组大小)</p>
-     * <p>对象8字节对齐填充 = 4byte</p>
+     * <p>对象头 16 byte = 8(类引用) + 4(头) + 4(数组大小)</p>
+     * <p>对象实例 =  8*n byte</p>
+     * <p>对象 padding = 0 byte </p>
      */
     @Test
-    public void testLongArraySize() throws InterruptedException {
-        int size = 1000000;
-        List<long[]> tmp = new ArrayList<>(size);
+    public void testLongArraySize() {
+        out.println(VM.current().details());
+        int size = 9;
         for (int i = 0; i < size; i++) {
-            tmp.add(new long[8]); //拥有8个long的数组，每个long占用8个byte字节，共64byte
+            out.println(ClassLayout.parseInstance(new long[i]).toPrintable());
         }
-        Thread.currentThread().join();
     }
+
+    /**
+     * <pre>
+     * # VM mode: 64 bits
+     * # Compressed references (oops): disabled
+     * # Compressed class pointers: 0-bit shift and 0x800000000 base
+     * # Object alignment: 8 bytes
+     * #                       ref, bool, byte, char, shrt,  int,  flt,  lng,  dbl
+     * # Field sizes:            8,    1,    1,    2,    2,    4,    4,    8,    8
+     * # Array element sizes:    8,    1,    1,    2,    2,    4,    4,    8,    8
+     * # Array base offsets:    16,   16,   16,   16,   16,   16,   16,   16,   16
+     * </pre>
+     * <p>对象头 16 byte = 8(类引用) + 4(头) + 4(数组大小)</p>
+     * <pre>
+     *      1. -XX:+UseCompressedOops 对象实例 =  4*n byte
+     *      2. -XX:-UseCompressedOops 对象实例 =  8*n byte
+     * </pre>
+     * <p>对象 padding = 0 byte </p>
+     */
+    @Test
+    public void testObjectArraySize() {
+        out.println(VM.current().details());
+        int size = 9;
+        for (int i = 0; i < size; i++) {
+            out.println(ClassLayout.parseInstance(new Object[i]).toPrintable());
+        }
+    }
+
 
 }

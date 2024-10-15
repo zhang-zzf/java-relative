9baf10d93b65f95dd107baf2ec94e1254d17b36b
zzf.zhang <zhangzhanfeng@icloud.com>
2024-10-15 18:53:59 +0800

    feat: add more Date test

--- java-date/src/test/java/com/github/learn/java_date/DateTest.java
+++ java-date/src/test/java/com/github/learn/java_date/DateTest.java
@@ -22,9 +22,65 @@ public class DateTest {
 
     /**
      * Date 101
+     * <pre>
+     *     类Date表示一个特定的时间瞬间，精度为毫秒。既然表示的是瞬间/时刻，那它必然和时区是无关的
+     *     确切的说：Date对象里存的是自格林威治时间（GMT）1970年1月1日0点至Date所表示时刻所经过的毫秒数，是个数值
+     * </pre>
      */
     @Test
     void givenDate_when101_then() {
+        // now 内部仅包含 unix timestamp
+        // timestamp 是时区无关的
+        Date now = new Date();
+        //
+        // now.toString() 会根据当前环境的默认时区输出与时区关联的当地时间字符串
+        // example: Tue Oct 15 17:57:52 CST 2024
+        then(now.toString()).isNotNull();
+        //
+        // unix 时间戳
+        long timestamp = now.getTime();
+        then(timestamp).isGreaterThan(0L);
+        //
+        // Date -> String
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        // 2024-10-15T18:01:03.981+08:00
+        String dateString = df.format(now);
+        then(dateString).isNotNull();
+    }
+
+
+    @Test
+    void givenTimeZone_when_then() {
+        // 默认时区
+        TimeZone tz = TimeZone.getDefault();
+        // sun.util.calendar.ZoneInfo[id="Asia/Shanghai",offset=28800000,dstSavings=0,useDaylight=false,transitions=31,lastRule=null]
+        then(tz).isNotNull();
+        then(TimeZone.getTimeZone("UTC").getID()).isEqualTo("UTC");
+        // 如何获取时区 ID
+        // UTC, Universal, W-SU, WET, Zulu, EST, HST, MST, ACT, AET, AGT, ART, AST, BET, BST, CAT, CNT, CST, CTT, EAT, ECT, IET, IST, JST, MIT, NET, NST, PLT, PNT, PRT, PST, SST, VST
+        // 洲/城市 -> Asia/Shanghai, America/New_York
+        // 没有 Asia/Beijing id
+        log.info("tz -> {}", (Object) TimeZone.getAvailableIDs());
+        //
+        // GMT+08:00 通用时区表示法
+        // 注意 GMT+08:00 不能使用 UTC+08:00 代替
+        // 东8区
+        then(TimeZone.getTimeZone("GMT+08:00").getOffset(0)).isEqualTo(28800000L);
+        then(TimeZone.getTimeZone("Asia/Shanghai").getOffset(0)).isEqualTo(28800000L);
+        // 西5区
+        then(TimeZone.getTimeZone("GMT-05:00").getOffset(0)).isEqualTo(-18000000L);
+        then(TimeZone.getTimeZone("America/New_York").getOffset(0)).isEqualTo(-18000000L);
+    }
+
+
+    @Test
+    void givenDate_whenToZoneString_then() {
+        Date utcZero = new Date(0);
+        SimpleDateFormat defaultZoneDf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        then(defaultZoneDf.format(utcZero)).isEqualTo("1970-01-01T08:00:00.000+08:00");
+        SimpleDateFormat nyDf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        nyDf.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        then(nyDf.format(utcZero)).isEqualTo("1969-12-31T19:00:00.000-05:00");
     }
 
     /**
@@ -62,6 +118,37 @@ public class DateTest {
         mapper.setDefaultPropertyInclusion(Include.NON_NULL);
         String jsonStr = mapper.writeValueAsString(b);
         then(jsonStr).isEqualTo("{\"createdAt\":3600000}");
+    }
+
+    /**
+     * Date <-> String
+     * <pre>
+     *     结论：针对 C/S 模式的服务
+     *     1. Client 与 Server 在同一个时区(utc8)，无需做额外处理。前后端都默认采取一个默认时区（uct8）来操作时间。
+     *     2. Client 与 Server 在不同的时区，Client 分布在 utc0 / utc3 / utc8 时区，Server 部署在 utc0 区
+     *        1. Server 端处理 Client 的 String 时间时，需要使用 Client 所在的时区来 parse String 字符串
+     * </pre>
+     */
+    @SneakyThrows
+    @Test
+    void givenDate_whenString_then() {
+        // tz
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+        //
+        // String -> Date
+        // 业务时间 -> 前后端约定隐含表示的是 utc8 时区的时间
+        // String bizTime = "1970-01-01 08:00:00+08:00";
+        String bizTime = "1970-01-01 08:00:00";
+        // df 默认使用 TimeZone.getDefault() 时区
+        // df.setTimeZone(TimeZone.getDefault());
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date _zero_in_utc = df.parse(bizTime); // Thu Jan 01 08:00:00 CST 1970
+        then(_zero_in_utc.getTime()).isEqualTo(0L);
+        //
+        // Date -> String
+        // 隐含表示使用 默认时区 utc8 格式化字符串
+        String formatDateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(_zero_in_utc);
+        then(formatDateStr).isEqualTo(bizTime);
     }
 
     /**
@@ -185,7 +272,7 @@ public class DateTest {
         SimpleDateFormat dfUseDefaultZone = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
         then(dfUseDefaultZone.getTimeZone().getID()).isEqualTo("Asia/Riyadh");
         Date parseByDefaultZone = dfUseDefaultZone.parse("1970-01-01T08:00:00.000+08:00");
-        then(parseByDefaultZone.getTime()).isEqualTo(18000000L); // utc 时间为 3 点
+        then(parseByDefaultZone.getTime()).isEqualTo(18000000L); // utc 时间为 5 点
         //
     }
 
@@ -299,7 +386,8 @@ public class DateTest {
         // XX 解析 +0800
         then(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXX").parse("1970-01-01T08:00:00+0800").getTime()).isEqualTo(0L);
         // XXX 解析 +08:00
-        then(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("1970-01-01T08:00:00+08:00").getTime()).isEqualTo(0L);
+        then(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX").parse("1970-01-01T08:00:00+08:00").getTime()).isEqualTo(
+            0L);
     }
 
     /**

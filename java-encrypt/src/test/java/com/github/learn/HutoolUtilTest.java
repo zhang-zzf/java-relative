15ce8a92bc2323c98af29402bc0131f1fec059c1
zhangzhanfeng <zhangzhanfeng@100.me>
2024-10-22 17:27:10 +0800

    feat: add HutoolUtilTest UT

--- java-encrypt/src/test/java/com/github/learn/HutoolUtilTest.java
+++ java-encrypt/src/test/java/com/github/learn/HutoolUtilTest.java
@@ -1,21 +1,28 @@
 package com.github.learn;
 
 import cn.hutool.crypto.digest.DigestUtil;
+import java.nio.charset.StandardCharsets;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.Base64;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
+@Slf4j
 public class HutoolUtilTest {
 
-    @Test
-    public void givenHutool_whenMd5_then() {
-        String stationId = "8440";
-        String stationName = "商品来源关系专用0002";
-        byte[] md5Bytes = DigestUtil.md5(stationId + stationName);
-        String md5Str = Base64.getEncoder().encodeToString(md5Bytes);
-        then(md5Str).isNotBlank();
-
-    }
+  @SneakyThrows
+  @Test
+  public void givenHutool_whenMd5_then() {
+    String sn = "K212239P00051";
+    String stationId = "8440";
+    byte[] md5Bytes = DigestUtil.md5(stationId);
+    String md5Str = Base64.getEncoder().encodeToString(md5Bytes);
+    log.info("base64(md5({}) -> {}", stationId, md5Str);
+    then(md5Str).isNotBlank();
+    String auth = Base64.getEncoder().encodeToString((sn + ":" + md5Str).getBytes(StandardCharsets.UTF_8));
+    log.info("base64({}:{}) -> {}", sn, md5Str, auth);
+  }
 }

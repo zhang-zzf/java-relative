8f22ead5025632df178ef1de83d36da1e3f800c8
zzf.zhang <zhangzhanfeng@icloud.com>
2025-01-02 12:34:14 +0800

    feat: add guava UT

--- guava-learn/src/test/java/com/github/learn/guava/GuavaTest.java
+++ guava-learn/src/test/java/com/github/learn/guava/GuavaTest.java
@@ -1,31 +1,36 @@
 package com.github.learn.guava;
 
 
+import static com.google.common.base.CharMatcher.*;
 import static com.google.common.base.Objects.*;
 import static com.google.common.base.Preconditions.*;
 import static com.google.common.collect.Comparators.greatest;
 import static com.google.common.collect.Comparators.least;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Comparator.comparingInt;
 import static java.util.Comparator.naturalOrder;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.BDDAssertions.catchThrowable;
 import static org.assertj.core.api.BDDAssertions.then;
 
+import com.google.common.base.*;
 import com.google.common.collect.Comparators;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.google.common.collect.Sets.SetView;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
@@ -90,11 +95,12 @@ public class GuavaTest {
         Comparators.isInOrder(topN, naturalOrder);
     }
 
-    @Test void givenGuava_whenCollection_then() {
+    @Test
+    void givenGuava_whenCollection_then() {
         // ImmutableList<Integer> list = ImmutableList.of(1, 2, 3);
-        List<Integer> list = List.of(1, 2, 3);
+        List<Integer> list = List.of(1, 1, 3);
         // ImmutableSet<Integer> set = ImmutableSet.of(1, 1, 2);
-        Set<Integer> set = Set.of(1, 1, 2);
+        Set<Integer> set = Set.of(1, 2);
         // ImmutableSet<Integer> setCopyOf = ImmutableSet.copyOf(list); // set.asList();
         Set<Integer> setCopyOf = Set.copyOf(list);
         // ImmutableList<Integer> listCopyOf = ImmutableList.copyOf(set);
@@ -109,8 +115,45 @@ public class GuavaTest {
         // Sets
         Set<Integer> union = Sets.union(set, setCopyOf);
         // Set<Integer> union = Stream.concat(set.stream(), setCopyOf.stream()).collect(toSet());
-
     }
 
+    @Test
+    void givenGuava_whenString_then() {
+        // Stream.reduce
+        String joined = Joiner.on(";").join(Arrays.asList("foo", "bar", "baz"));
+        String[] splited = "foo, bar, baz".split(",");
+        // 保留所有 ascii 字符
+        CharMatcher.ascii().retainFrom(joined);
+        CharMatcher.javaIsoControl();
+        // trim
+        whitespace().trimFrom(joined);
+        whitespace().trimAndCollapseFrom(joined, '_');
+        CharMatcher.anyOf("*");
+    }
+
+
+    @Test
+    void givenGuava_whenHashing_then() {
+        // 无状态，线程安全
+        HashFunction sha512 = Hashing.sha512();
+        HashCode hashCode = sha512.hashString("foo", UTF_8);
+        byte[] bytes = hashCode.asBytes();
+        // 有状态，线程栈封闭（方法内使用）
+        sha512.newHasher()
+            .putByte((byte) 1)
+            .putString("foo", UTF_8)
+            .putBytes(bytes)
+            .hash()
+        ;
+        //
+        // BloomFilter
+        BloomFilter<CharSequence> bf = BloomFilter.create(Funnels.stringFunnel(UTF_8), 1000000, 0.0001);
+        for (int i = 0; i < 10000; i++) {
+            bf.put("" + i);
+        }
+        for (int i = 0; i < 10000; i++) {
+            bf.mightContain("" + i);
+        }
+    }
 
 }
